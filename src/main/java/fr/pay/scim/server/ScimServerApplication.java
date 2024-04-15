c79ae34f6dcf277e45bd536e4a5eff391fc902d6
patrice <cora.paubry@gmail.com>
2024-04-15 22:28:38 +0200

    structure springboot vide

---
+++ src/main/java/fr/pay/scim/server/ScimServerApplication.java
@@ -0,0 +1,13 @@
+package fr.pay.scim.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScimServerApplication.class, args);
+	}
+
+}
