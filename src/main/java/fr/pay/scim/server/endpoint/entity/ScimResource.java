fc96b31849008f2336df1ad5b8854ed4cdcc4c73
patrice <cora.paubry@gmail.com>
2024-04-16 21:38:01 +0200

    Live reload + correction ScimResource

--- src/main/java/fr/pay/scim/server/endpoint/entity/ScimResource.java
+++ src/main/java/fr/pay/scim/server/endpoint/entity/ScimResource.java
@@ -2,8 +2,21 @@ package fr.pay.scim.server.endpoint.entity;
 
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data()
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
+@Schema(name = "resource")
 public class ScimResource {
 
 	/**
